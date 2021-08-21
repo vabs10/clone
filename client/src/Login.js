54fa7944c531b489fa6f43592110921c7652d711
Vaibhav Yagneshwar <vyagneshwar98@gmail.com>
2021-08-21 12:04:36 +0530

    clone

---
+++ client/src/Login.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Container } from "react-bootstrap"
+
+const AUTH_URL =
+  "https://accounts.spotify.com/authorize?client_id=fa6ff9c34dba4f0b9851aaf60f9e4f9f&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
+export default function Login() {
+  return (
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <a className="btn btn-success btn-lg" href={AUTH_URL}>
+        Login With Spotify
+      </a>
+    </Container>
+  )
+}
